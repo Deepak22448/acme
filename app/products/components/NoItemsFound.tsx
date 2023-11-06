e1f388e32d86086d9b3d34e553cc09d6e1ae3286
deepak22448 <deepaksandeep33@gmail.com>
2023-11-06 22:19:26 +0530

    made [NoItemsFound] response center vertically.

--- app/products/components/NoItemsFound.tsx
+++ app/products/components/NoItemsFound.tsx
@@ -1,6 +1,6 @@
 const NoItemsFound = () => {
   return (
-    <div className="w-full min-h-screen lg:min-h-min flex justify-center items-center text-purple-500 text-xl md:text-2xl">
+    <div className="w-full min-h-[calc(100vh-5rem)] lg:min-h-min flex justify-center items-center text-purple-500 text-xl md:text-2xl">
       <h2>NO ITEMS FOUND.</h2>
     </div>
   );
